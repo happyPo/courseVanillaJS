47ee1bbd63d4d17cfd8b8ce6367b382aa240068b
Eugene Braitsev <ebr@nemetos.net>
2019-11-11 12:01:38 +0200

    Formated

--- 14_RequestToServer/fetch.js
+++ 14_RequestToServer/fetch.js
@@ -12,36 +12,35 @@ sendRequest('GET', requestUrl)
 .catch(err =>console.log(err));
 */
 
-
 // =============================== GET & POST ==================================================
 
 function sendRequest(method, url, body = null) {
-    const headers = {
-        'Content-Type': 'application/json'
-    };
+  const headers = {
+    "Content-Type": "application/json"
+  };
 
-    return fetch(url, {
-        method: method,
-        body: JSON.stringify(body),
-        headers: headers
-    }).then(response => {
-        if (response.ok) {
-            return response.json()
-        }
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
 
-        return response.json().then(error=>{
-            const e = new Error('Something went wrong');
-            e.data = error;
-            throw e
-        })
-    })
+    return response.json().then(error => {
+      const e = new Error("Something went wrong");
+      e.data = error;
+      throw e;
+    });
+  });
 }
 
 const body = {
-    name: 'Eugene',
-    age: 31
+  name: "Eugene",
+  age: 31
 };
 
-sendRequest('POST', requestUrl, body)
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+sendRequest("POST", requestUrl, body)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
